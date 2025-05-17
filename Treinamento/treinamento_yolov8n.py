dd49a09a7a571d7b6f74ce30fb21324f1e0500dd
Dayane Damaceno <dayane.damaceno@hotmail.com>
2025-05-16 21:59:22 -0300

    Arquivos de treinamento

---
+++ Treinamento/treinamento_yolov8n.py
@@ -0,0 +1,18 @@
+from ultralytics import YOLO
+import os
+
+
+arquivo_config = 'yolov8n/configs_modelo.yaml'
+
+model = YOLO('yolov8s.yaml')
+
+resultados = model.train(
+    data=arquivo_config, 
+    epochs=140, 
+    imgsz=640, 
+    name='yolov8s_modelo')
+
+dir_resultado = os.path.join('runs', 'detect', 'yolov8s_modelo')
+print(f"Resultados salvos em: {dir_resultado}")
+
+
